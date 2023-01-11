df6ea6374a37d34dbd41799b969b50093e7138f3
= <spriyasathish03@gmail.com>
2023-01-11 17:25:00 +0530

    Commit for routes and API Doc file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   #root "application#index"
-  root "pages#home"
-  get "about" , to: "pages#about"
+  #root "pages#home"
+  #get "about" , to: "pages#about"
+  root "movies#index"
 end
